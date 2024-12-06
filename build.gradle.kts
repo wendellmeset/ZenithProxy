24dd3d36041f33d501807aea6d6432419c42a4e2
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-12-06 14:22:42 -0800

    update mcpl

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     implementation("com.github.rfresh2.discord4j:discord4j-core:3.4.4.11") {
         exclude(group = "io.netty")
     }
-    implementation("com.github.rfresh2:MCProtocolLib:1.21.4.1") {
+    implementation("com.github.rfresh2:MCProtocolLib:1.21.4.3") {
         exclude(group = "io.netty")
     }
     val nettyVersion = "4.1.115.Final"
